dfdbef70f48bcaa7a2e01c88fc186c1df2f95463
Minh20812 <blossworld12@gmail.com>
2024-08-26 11:59:56 +0700

    Follow 36:00 video 3

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,8 +1,56 @@
 import User from "../models/userModel.js";
 import asyncHandler from "../middlewares/asynHandler.js";
+import bcrypt from "bcrypt";
+import createToken from "../utils/createToken.js";
 
 const createUser = asyncHandler(async (req, res) => {
-  res.send("hello");
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    throw new Error("Please provide all the required fields");
+  }
+
+  const userExists = await User.findOne({ email });
+  if (userExists) res.status(400).send("User already exists");
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const newUser = new User({ username, email, password: hashedPassword });
+
+  try {
+    await newUser.save();
+    createToken(res, newUser._id);
+
+    res.status(201).json({
+      _id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+    });
+  } catch (err) {
+    res.status(400).send(err);
+    throw new Error(err);
+  }
+});
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (isPasswordValid) {
+      createToken(res, existingUser._id);
+      res.status(201).json({
+        _id: existingUser._id,
+        username: existingUser.username,
+        email: existingUser.email,
+        isAdmin: existingUser.isAdmin,
+      });
+      return;
+    }
+  }
 });
 
-export { createUser };
+export { createUser, loginUser };
